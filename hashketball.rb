edbbc2798fedc90d69b41d4594fa0a04386285c3
Aysan Isayo <aisayo@icloud.com>
2019-06-03 21:42:40 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -170,7 +170,6 @@ def player_numbers(team_name)
   #returns array of jersey numbers for that team 
   game_hash.map do |place, team|
     if team[:team_name] == team_name
-      binding.pry
       team.each do |attributes, data|
         if attributes == :players
           data.each do |player|
