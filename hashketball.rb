a3041989fec5ac19f7ecce08a81780cf76cf9235
Aysan Isayo <aisayo@icloud.com>
2019-06-03 21:26:41 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -146,7 +146,9 @@ def game_hash
 
 def team_colors(team_name)
   game_hash.each do |place, team|
-    binding.pry
+    if team[:team_name] == team_name
+      return team[:colors]
+    end 
   end 
 end 
 
