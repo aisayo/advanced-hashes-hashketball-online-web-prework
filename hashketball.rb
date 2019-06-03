d66eeb59c1f0a71848d3085ce1453e7be20472ee
Aysan Isayo <aisayo@icloud.com>
2019-06-03 21:40:44 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -172,7 +172,10 @@ def player_numbers(team_name)
     if team[:team_name] == team_name
       team.each do |attributes, data|
         if attributes == :players
-          
+          data.each do |player|
+            binding.pry
+            player[:number]
+          end 
         end 
       end 
     end
