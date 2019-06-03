b02a08f4c83feebaeced77b706a1fa5acacd2e1f
Aysan Isayo <aisayo@icloud.com>
2019-06-03 21:57:39 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -204,7 +204,8 @@ def player_stats(players_name)
       if attributes == :players
         data.each do |player|
           if player[:player_name] == players_name
-            new_hash = player.delete_if do 
+            new_hash = player.delete_if do |k, v|
+              k == :player_name
           end 
         end 
       end 
