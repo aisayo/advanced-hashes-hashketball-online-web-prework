5ac38c5a0f2918d6f8690f23dc7984d8402c18aa
Aysan Isayo <aisayo@icloud.com>
2019-06-03 21:23:09 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -130,7 +130,18 @@ def game_hash
    end 
  end 
  
- def shoe_size 
+ def shoe_size(players_name)
+   game_hash.each do |place, team| 
+     team.each do |attribute, data|
+       if attribute == :players 
+         data.each do |player|
+           if player[:player_name] == players_name
+             return player[:points]
+           end
+         end 
+       end
+     end 
+   end 
  end 
 
 
